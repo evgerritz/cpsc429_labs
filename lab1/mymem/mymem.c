36d9d7ad3cfc48f14836a6f52a185dcbce9d53f0
Evan Gerritz <evgerritz@gmail.com>
2022-09-15 17:28:09 -0400

    added comments

--- lab1/mymem/mymem.c
+++ lab1/mymem/mymem.c
@@ -9,9 +9,12 @@ static dev_t first_dev_num = -1;
 static struct cdev char_dev;
 static struct class * dev_class;
 
+// pointer to buffer in kernel
 static char * buffer;
+// current location in buffer
 static size_t pos;
 
+// fill out file_operations struct with implementations
 static struct file_operations fops = {
     .owner = THIS_MODULE,
     .open = mymem_open,
@@ -21,6 +24,7 @@ static struct file_operations fops = {
     .release = mymem_close,
 };
 
+// sets the permissions on the created device file to "rw-rw-rw-"
 static char * mymem_devnode(struct device *dev, umode_t *mode) {
     if (!mode) return NULL;
     if (dev->devt == first_dev_num) {
@@ -29,45 +33,53 @@ static char * mymem_devnode(struct device *dev, umode_t *mode) {
     return NULL;
 }
 
-// module init function
+// initialize the kernel module
 static int __init mymem_init(void) {
     int ret_val, i;
     struct device * mymem_device;
     printk(KERN_ALERT "Module mymem init!\n");
     
+    // allocate space for the character device
     ret_val = alloc_chrdev_region(&first_dev_num, 0, 1, NAME);
     if (ret_val < 0) return ret_val;
 
+    // create a device class named "chardrv" 
     if (IS_ERR(dev_class = class_create(THIS_MODULE, "chardrv"))) {
         unregister_chrdev_region(first_dev_num, 1);
 	    return PTR_ERR(dev_class);
     }
+    // link class to permissions setting function
     dev_class->devnode = mymem_devnode;
 
+    // try to create actual device file
     if (IS_ERR(mymem_device = device_create(dev_class, NULL, first_dev_num, NULL, NAME))) {
         class_destroy(dev_class);
         unregister_chrdev_region(first_dev_num, 1);
         return PTR_ERR(mymem_device);
     }
 
+    // initialize the device and add it to the VFS
     cdev_init(&char_dev, &fops);
     ret_val = cdev_add(&char_dev, first_dev_num, 1);
+
+    // allocate buffer in the kernel
     buffer = kmalloc(BUF_SIZE, GFP_USER);    
     if (ret_val < 0 || buffer == NULL) {
         device_destroy(dev_class, first_dev_num) ;
         class_destroy(dev_class);
         unregister_chrdev_region(first_dev_num, 1);
-	return (ret_val < 0) ? ret_val : -ENOMEM;
+        return (ret_val < 0) ? ret_val : -ENOMEM;
     }
 
     // clear the buffer
     for (i = 0; i < BUF_SIZE; i++) {
-   	buffer[i] = '\0'; 
+        buffer[i] = '\0'; 
     }
     return 0;
 }
 
 static void __exit mymem_exit(void) {
+    // deallocate and delete all assoc. data structures
     cdev_del(&char_dev);
     device_destroy(dev_class, first_dev_num);
     class_destroy(dev_class);
@@ -100,6 +112,7 @@ static ssize_t mymem_read(struct file *filp, char __user *user_buffer, size_t le
     if (pos + length > BUF_SIZE) {
         return -EINVAL;
     } else {
+        // copy_to_user returns number of bytes not copied
         bytes_read = length - copy_to_user(user_buffer,  buffer+pos, length);
     }
     pos += length;
@@ -114,7 +127,8 @@ static ssize_t mymem_write(struct file *filp, const char *user_buffer, size_t le
     if (pos + length > BUF_SIZE) {
         return -EINVAL;
     } else {
-          bytes_written = length - copy_from_user(buffer+pos, user_buffer, length);
+        // copy_from_user returns number of bytes not copied
+        bytes_written = length - copy_from_user(buffer+pos, user_buffer, length);
     }
     pos += bytes_written;
     printk("new pos: %zu\n", pos);
@@ -127,14 +141,14 @@ static loff_t mymem_llseek(struct file *filp, loff_t offset, int whence) {
     switch(whence) {
          case 0: // SEEK_SET 
      	      newpos = offset;
-	      break;
-	 case 1: // SEEK_CUR
-	      newpos = pos + offset;
-	      break;
-  	 case 2: // SEEK_END
-	      newpos = BUF_SIZE + offset;
-	      break;
-	 default:
+              break;
+         case 1: // SEEK_CUR
+              newpos = pos + offset;
+              break;
+         case 2: // SEEK_END
+              newpos = BUF_SIZE + offset;
+              break;
+         default:
               return -EINVAL;
     } 
     if (newpos < 0) return -EINVAL;
--- lab1/mymem/test.c
+++ lab1/mymem/test.c
@@ -7,17 +7,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-int basic() {
-    int fd = open("/dev/mymem", O_RDWR); 
-    write(fd, "test123", 7);
-    lseek(fd, 0, SEEK_SET);
-    char buf[512];
-    read(fd, buf, 7);
-    printf("%s\n", buf);    
-    close(fd);
-    return 0;
-}
 
+// struct to hold both the avg read and write times in microseconds
+// for each number of bytes
 struct rw_time
 {
     double read;
@@ -25,13 +17,16 @@ struct rw_time
 };
 
 
+// gets time measurements for reads/writes of size num_bytes and
+// fills out an rw_time struct
 struct rw_time * time_to_read_write(size_t num_bytes) {
-    char * buf_to_wrt = malloc(num_bytes);
-    char * buf_to_rd = malloc(num_bytes);
-    char i;
-    int trial;
     const int TRIALS = 1000;
     clock_t start;
+    char i;
+    int trial;
+
+    char * buf_to_wrt = malloc(num_bytes);
+    char * buf_to_rd = malloc(num_bytes);
     struct rw_time * times = malloc(sizeof(struct rw_time));
 
     int old;
@@ -39,34 +34,36 @@ struct rw_time * time_to_read_write(size_t num_bytes) {
     size_t total_diff_wrt = 0;
     size_t total_diff_rd = 0;
     for (trial = 0; trial < TRIALS; trial++) {
-	for (i = 0; i < num_bytes; i++) {
-	     // generate random buffer
-	     buf_to_wrt[i] = (char) rand() % 255;
-	}
+        // generate random buffer, to ensure no caching between trials
+        for (i = 0; i < num_bytes; i++) {
+             buf_to_wrt[i] = (char) rand() % 255;
+        }
 
-	lseek(fd, 0, SEEK_SET);
-	start = clock();
-	write(fd, buf_to_wrt, num_bytes);
-	old = total_diff_wrt;
-	total_diff_wrt += clock() - start;
-	assert(total_diff_wrt - old >= 0);
+        //seek back to beginning
+        lseek(fd, 0, SEEK_SET);
+        start = clock();
+        write(fd, buf_to_wrt, num_bytes);
+        old = total_diff_wrt;
+        total_diff_wrt += clock() - start;
+        assert(total_diff_wrt - old >= 0);
 
-	lseek(fd, 0, SEEK_SET);
-	start = clock();
-	read(fd, buf_to_rd, num_bytes);
-	old = total_diff_rd;
-	total_diff_rd += clock() - start;
-	assert(total_diff_rd - old >= 0);
+        lseek(fd, 0, SEEK_SET);
+        start = clock();
+        read(fd, buf_to_rd, num_bytes);
+        old = total_diff_rd;
+        total_diff_rd += clock() - start;
+        assert(total_diff_rd - old >= 0);
 
-	#if DEBUG
-	for (i = 0; i < num_bytes; i++) {
-	    assert(buf_to_wrt[i] == buf_to_rd[i]); 
-	}
-	#endif
+        #if DEBUG
+        for (i = 0; i < num_bytes; i++) {
+            assert(buf_to_wrt[i] == buf_to_rd[i]); 
+        }
+        #endif
     }
     times->write = total_diff_wrt / (CLOCKS_PER_SEC * TRIALS/1000000);
     times->read = total_diff_rd / (CLOCKS_PER_SEC * TRIALS/1000000);
 
+    // close /dev/mymem and free buffers
     close(fd);
     free(buf_to_wrt);
     free(buf_to_rd);
@@ -77,15 +74,18 @@ struct rw_time * time_to_read_write(size_t num_bytes) {
 #define NUM_SIZES 5
 
 int main () {
+    // initialize array of sizes in bytes of the operations
     int sizes[NUM_SIZES] = {1, 64, 1024, 64*1024, 512*1024};
     struct rw_time * times[NUM_SIZES]; 
     int i;
     for (i = 0; i < NUM_SIZES; i++) {
         times[i] = time_to_read_write(sizes[i]);
-	printf("%d\t:%f\t%f\n", sizes[i], times[i]->read, times[i]->write);
+        printf("%d\t:%f\t%f\n", sizes[i], times[i]->read, times[i]->write);
     }
+
+    // free the rw_time structs
     for (i = 0; i < NUM_SIZES; i++) {
-	free(times[i]);
+        free(times[i]);
     }
     return 0;
 }
