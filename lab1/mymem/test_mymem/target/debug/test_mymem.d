b05bbc6d10b6e02c4ac87d40d872a6b55dd6f979
Evan Gerritz <evgerritz@gmail.com>
2022-10-03 00:47:17 +0000

    added compiled kernel deb files

--- lab1/mymem/test_mymem/target/debug/test_mymem.d
+++ lab1/mymem/test_mymem/target/debug/test_mymem.d
@@ -1,1 +1,1 @@
-/home/evgerritz/Dropbox/Yale/CPSC429/cpsc429_labs/lab1/mymem/test_mymem/target/debug/test_mymem: /home/evgerritz/Dropbox/Yale/CPSC429/cpsc429_labs/lab1/mymem/test_mymem/src/main.rs
+/home/evgerritz/cpsc429_labs/lab1/mymem/test_mymem/target/debug/test_mymem: /home/evgerritz/cpsc429_labs/lab1/mymem/test_mymem/src/main.rs
