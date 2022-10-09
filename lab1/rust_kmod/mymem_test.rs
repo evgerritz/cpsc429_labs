18872e174a34cbf2676f95e02296543111f1bbaa
Evan Gerritz <evgerritz@gmail.com>
2022-10-09 19:04:42 -0400

    checking assertion

--- lab1/rust_kmod/mymem_test.rs
+++ lab1/rust_kmod/mymem_test.rs
@@ -117,15 +117,15 @@ fn time_to_read_write(num_bytes: usize) -> Result<RWTime> {
         unsafe { bindings::ktime_get_ts64(&mut start); }
         let n = buffer.write(&buf_to_wrt, 0);
         unsafe { bindings::ktime_get_ts64(&mut end); }
-        pr_info!("{:?}\t{:?}", end.tv_sec - start.tv_sec, end.tv_nsec - start.tv_nsec);
-        assert!(n == num_bytes);
+        pr_info!("{:?}\t{:?}\t{:?}", n, num_bytes,  end.tv_sec - start.tv_sec, end.tv_nsec - start.tv_nsec);
+        //assert!(n == num_bytes);
         //total_wrt_time += cpu_time.subsec_micros() as u64;
 
         unsafe { bindings::ktime_get_ts64(&mut start); }
         let n = buffer.read(&mut buf_to_rd, 0);
         unsafe { bindings::ktime_get_ts64(&mut end); }
         pr_info!("{:?}\t{:?}", end.tv_sec - start.tv_sec, end.tv_nsec - start.tv_nsec);
-        assert!(n == num_bytes);
+        //assert!(n == num_bytes);
         //total_rd_time += cpu_time2.subsec_micros() as u64;
 
         for i in 0..num_bytes {
