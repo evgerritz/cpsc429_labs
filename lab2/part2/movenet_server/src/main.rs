a5d0d8832260681095f0828a78faae9e16fea210
Evan Gerritz <evgerritz@gmail.com>
2022-10-25 00:26:18 -0400

    interpreter setup

--- lab2/part2/movenet_server/src/main.rs
+++ lab2/part2/movenet_server/src/main.rs
@@ -1,10 +1,11 @@
 use std::net::{TcpListener, TcpStream};
+use tflitec::interpreter::{Interpreter, Options};
 use std::io::Read;
 use std::io::Write;
 use std::thread;
 
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client(mut stream: TcpStream, interpeter: &Interpreter) {
     const IM_SIZE: usize = 384 * 288;
     let mut image_bytes: [u8; IM_SIZE];
 
@@ -17,18 +18,25 @@ fn handle_client(mut stream: TcpStream) {
 
     // get output
     let output_tensor = interpreter.output(0).unwrap();
-    let tensor_data = output_tensor.data::<f32>()
+    let tensor_data = output_tensor.data::<f32>();
 
     stream.write(&tensor_data[..]).unwrap();
 }
 
 pub fn main() {
-    let listener: TcpListener = TcpListener::bind("10.0.2.15:8888").unwrap();
+	let options = Options::default();
+	let path = format!("resource/lite-model_movenet_singlepose_lightning_tflite_int8_4.tflite");
+	let interpreter = Interpreter::with_model_path(&path, Some(options)).unwrap();
+	interpreter.allocate_tensors().expect("Allocate tensors [FAILED]");
+
+    let listener: TcpListener = TcpListener::bind("0.0.0.0:8888").unwrap();
+    // use this for zoo:
+    //let listener: TcpListener = TcpListener::bind("10.0.2.15:8888").unwrap();
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                thread::spawn(move || {
-                    handle_client(stream);
+                thread::spawn(|| {
+                    handle_client(stream, &interpeter);
                 });
             }
             Err(_) => {
