18df2cc7361f5e7d18f44901c91b0fdc88c1a225
Evan <evgerritz@gmail.com>
2022-11-12 20:54:54 +0000

    copied old develop files

--- lab2/part4/rust_kmod/compile_module.sh
+++ lab2/part4/rust_kmod/compile_module.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-SHARED_SAMPLES=~/cpsc429_labs/lab1/rust_kmod
-BUILD_SAMPLES=~/linux3/samples/rust
-BUILD_DIR=~/linux3/
-CURRENT_BRANCH=1_p5
+SHARED_SAMPLES=~/cpsc429_labs/lab2/part4/rust_kmod
+BUILD_SAMPLES=~/linux/samples/rust
+BUILD_DIR=~/linux/
+CURRENT_BRANCH=2_p4
 
 cd $SHARED_SAMPLES
 git pull origin $CURRENT_BRANCH
-cp -f $SHARED_SAMPLES/rust_mymem.rs  $BUILD_SAMPLES/
-cp -f $SHARED_SAMPLES/mymem_test.rs  $BUILD_SAMPLES/
-cp -f $SHARED_SAMPLES/Makefile  $BUILD_SAMPLES/
+cp -f $SHARED_SAMPLES/custom_modules/rust_camera.rs  $BUILD_SAMPLES/custom_modules/
 
-cd $BUILD_DIR
-KRUSTFLAGS="--emit=metadata --extern mymem=$BUILD_SAMPLES/librust_mymem.rmeta" make SUBDIRS=$BUILD_SAMPLES modules -j4 #2>&1 | less
-cp -f $BUILD_SAMPLES/rust_mymem.ko $SHARED_SAMPLES/
-cp -f $BUILD_SAMPLES/mymem_test.ko $SHARED_SAMPLES/
+cd $BUILD_DIR/custom_modules/
+make camera
+#KRUSTFLAGS="--emit=metadata --extern mymem=$BUILD_SAMPLES/librust_mymem.rmeta" make SUBDIRS=$BUILD_SAMPLES modules -j4 #2>&1 | less
+cp -f $BUILD_SAMPLES/custom_modules/rust_camera.ko $SHARED_SAMPLES/custom_modules/
 
 cd $SHARED_SAMPLES
 git checkout $CURRENT_BRANCH
-git add rust_mymem.ko
-git add mymem_test.ko
-git commit -m "rebuilt mymem.ko and mymem_test.ko"
+git add custom_modules/rust_camera.ko
+git commit -m "rebuilt rust_camera.ko"
 git push origin $CURRENT_BRANCH
