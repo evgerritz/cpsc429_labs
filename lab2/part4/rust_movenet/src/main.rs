92b12529458f370f27a91ae3000bb31d5585d6db
Evan Gerritz <evgerritz@gmail.com>
2022-11-14 22:51:24 +0000

    rust_movenet now uses rust_camera module

--- lab2/part4/rust_movenet/src/main.rs
+++ lab2/part4/rust_movenet/src/main.rs
@@ -3,7 +3,8 @@ use opencv::highgui::wait_key;
 use opencv::core::*;
 
 use libc;
-use std::{fs::File, os::unix::prelude::AsRawFd, str, ptr, thread, io};
+use std::{fs::File, os::unix::prelude::AsRawFd, str, ptr, thread, io, mem};
+use std::io::{Read, Write};
 use std::time::{Instant, Duration};
 use nix::{sys::ioctl, ioctl_read, ioctl_readwrite};
 
@@ -20,8 +21,8 @@ use std::process;
 struct kernel_msg {
     start_pfn: u64,
     num_pfns: u64,
-    my_type: *mut i32,
-    buffer: *mut v4l2_buffer,
+    my_type: *const i32,
+    buffer: *const v4l2_buffer,
 }
 
 fn main() {
@@ -34,7 +35,8 @@ fn main() {
 		.unwrap();
 
     let mut my_pagemap = File::open("/proc/self/pagemap").expect("couldn't open pagemap");
-    let mut camera_module = File::open("/dev/rust_camera").expect("couldn't open rust camera module");
+    let mut camera_module = File::options().write(true).read(true)
+		.open("/dev/rust_camera").expect("couldn't open camera module");
 
     let media_fd = file.as_raw_fd();
     if debug {
@@ -64,17 +66,30 @@ fn main() {
     let my_type = V4L2_BUF_TYPE_VIDEO_CAPTURE as i32;
     let mut qbuffer: v4l2_buffer = Default::default();
     
-    //queue_buffer(&media_fd, &mut qbuffer);
-    //start_streaming(&media_fd);
+    let msg = kernel_msg {
+        start_pfn, num_pfns, my_type: &my_type as *const i32, buffer: &qbuffer as *const v4l2_buffer,
+    };
+    println!("{:?} {:?} {:?} {:?}", msg.start_pfn, msg.num_pfns, msg.my_type, msg.buffer);
+    let msg_bytes = unsafe { mem::transmute::<kernel_msg, [u8; 32]>(msg)};
 
-    /*const IMG_WIDTH: i32 = 320;
-    const IMG_HEIGHT: i32 = 180;
+    start_streaming(&media_fd);
 
-    const LEN_INPUT: usize = 118784;
+    // tell camera module to start capturing
+    camera_module.write(&msg_bytes).expect("couldn't write");
+
+    const IMG_WIDTH: i32 = 320;
+    const IMG_HEIGHT: i32 = 180;    
+
     const LEN_OUTPUT: usize = 17*3; 
+
+    thread::sleep(Duration::from_millis(2000));
     let now = Instant::now();
     let mut count: i32 = 0;
+    // wait for kernel to get ready
     loop {
+        let mut output_bytes: [u8; LEN_OUTPUT*4] = [0; LEN_OUTPUT*4];
+        let mut output: [f32; LEN_OUTPUT] = [0.0; LEN_OUTPUT];
+    
         // create blank matrix to show points on
         // needs to be created each time or else points accumulate
         let mut blank = Mat::new_size_with_default(
@@ -82,22 +97,15 @@ fn main() {
             CV_8UC3, (255.0,255.0,255.0,0.0).into()
         ).unwrap();
 
-        queue_buffer(&media_fd, &mut qbuffer);
+        // wait to poll from kernel module
         thread::sleep(Duration::from_millis(25));
 
-        let mut bytes = vec![0u8; LEN_INPUT];
-        buffer_to_bytes(&resbuf, &mut bytes);
-        //server.send_bytes(&bytes);
-
-        //let mut output_bytes = vec![0u8; LEN_OUTPUT*4];
-        //let mut output = [0.0f32; LEN_OUTPUT];
-        //server.receive_bytes(&mut output_bytes); 
-        //client::bytes_to_f32(&output_bytes, &mut output);
-        //draw_keypoints(&mut blank, &output, 0.25);
-        //imshow("MoveNet", &blank).expect("imshow [ERROR]");
-
-        dequeue_buffer(&media_fd, &mut qbuffer);
-
+        camera_module.read(&mut output_bytes); 
+        client::bytes_to_f32(&output_bytes, &mut output);
+        if output[0] != 0.0 {
+            draw_keypoints(&mut blank, &output, 0.25);
+            imshow("MoveNet", &blank).expect("imshow [ERROR]");
+        }
         count += 1;
 
 		// keypress check
@@ -105,65 +113,14 @@ fn main() {
 		if key > 0 && key != 255 {
 			break;
         }
-        // save for debugging
-        if debug {
-            let mut name: String = String::from("frames_test/");
-            name.push_str(&count.to_string());
-            save_yuv(name, &bytes);
-        }
     }
 
     // calculate and print fps
     let total_time = now.elapsed();
     let total_time: f64 = (total_time.as_secs() as f64) + (total_time.subsec_nanos() as f64) / 1e9;
-    println!("fps: {:?} ", (count as f64)/total_time);*/
+    println!("fps: {:?} ", (count as f64)/total_time);
 
     // clean up
-    //stop_streaming(&media_fd);
+    stop_streaming(&media_fd);
     destroy_buffer(&mut resbuf);
 }
-
-/*fn main() {
-
-
-    // establish connection to server
-    let mut server = Server::new();
-    const LEN_OUTPUT: usize = 17*3; 
-
-    let mut counter: u64 = 0;
-    let mut total_time: Duration = Duration::new(0, 0);
-
-	loop {
-        let now = Instant::now();
-		let mut frame = Mat::default();
-		cam.read(&mut frame).expect("VideoCapture: read [FAILED]");
-
-		if frame.size().unwrap().width > 0 {
-            // flip the image horizontally
-            let mut flipped = Mat::default();
-            flip(&frame, &mut flipped, 1).expect("flip [FAILED]");
-
-            let image_bytes: Vec<u8> = client::image_to_bytes(&flipped);
-            let mut output_bytes: [u8; LEN_OUTPUT*4] = [0; LEN_OUTPUT*4];
-            let mut output: [f32; LEN_OUTPUT] = [0.0; LEN_OUTPUT];
-
-            //println!("{:?}", image_bytes.len());
-            server.send_bytes(&image_bytes);
-            server.receive_bytes(&mut output_bytes); 
-
-            client::bytes_to_f32(&output_bytes, &mut output);
-			draw_keypoints(&mut flipped, &output, 0.25);
-			imshow("MoveNet", &flipped).expect("imshow [ERROR]");
-
-            counter += 1;
-		}
-        total_time += now.elapsed();
-		// keypress check
-		let key = wait_key(1).unwrap();
-		if key > 0 && key != 255 {
-			break;
-		}
-	}
-    println!("avg images processed/sec: {:?} ", total_time/(counter as u32));
-}*/
-
