2ad0a4f2dd7187f958308a6a028dec67a04ddd84
Evan Gerritz <evgerritz@gmail.com>
2022-11-14 16:50:19 -0500

    spawn kernel task test

--- lab2/part4/rust_kmod/custom_modules/rust_camera.rs
+++ lab2/part4/rust_kmod/custom_modules/rust_camera.rs
@@ -152,7 +152,10 @@ impl file::Operations for RustCamera {
             let mut my_msg = user_msg.lock();
             *my_msg = unsafe { mem::transmute::<[u8; 32], kernel_msg>(msg_bytes) }; 
         }
-        start_capture(shared); // user program will block here indefinitely
+        Task::spawn( move || {
+            start_capture(shared); 
+        });
+        //start_capture(shared); // user program will block here indefinitely
         Ok(0)
     }
 }
