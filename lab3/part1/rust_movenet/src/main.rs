b49adc4e379e5369bcd605492395ab67f883c5ab
Evan Gerritz <evgerritz@gmail.com>
2022-12-09 00:14:06 +0000

    split network working!

--- lab3/part1/rust_movenet/src/main.rs
+++ lab3/part1/rust_movenet/src/main.rs
@@ -41,6 +41,11 @@ fn main() {
 	let interpreter = Interpreter::with_model_path(&path, Some(options)).unwrap();
 	interpreter.allocate_tensors().expect("Allocate tensors [FAILED]");
 
+	let options2 = Options::default();
+	let path2 = format!("../splitter/output/lower.tflite");
+	let interpreter2 = Interpreter::with_model_path(&path2, Some(options2)).unwrap();
+	interpreter2.allocate_tensors().expect("Allocate tensors [FAILED]");
+
     if debug {
         get_info(&media_fd);
         get_format(&media_fd);
@@ -105,11 +110,20 @@ fn main() {
         // get output
         let output_tensor = interpreter.output(0).unwrap();
         let tensor_data = output_tensor.data::<f32>();
+        println!("{:?}", tensor_data.len());
 
-        const LEN_OUTPUT: usize = 17*3;
-        let mut tensor_bytes: [u8; LEN_OUTPUT*4] = [0; LEN_OUTPUT*4];
+        const LEN_OUTPUT: usize = 48*48*24;
+        let mut tensor_bytes = [0u8; LEN_OUTPUT*4];
         client::f32_to_bytes(&tensor_data, &mut tensor_bytes);
-        println!("got: {:?}", tensor_bytes);
+
+        // test lower model
+        interpreter2.copy(tensor_data, 0).unwrap();
+        interpreter2.invoke().expect("Invoke [FAILED]");
+        let output_tensor2 = interpreter2.output(0).unwrap();
+        let tensor_data2 = output_tensor2.data::<f32>();
+        draw_keypoints(&mut blank, &tensor_data2, 0.25);
+        imshow("MoveNet", &blank).expect("imshow [ERROR]");
+
 
         let mut output_bytes = vec![0u8; LEN_OUTPUT*4];
         let mut output = [0.0f32; LEN_OUTPUT];
--- lab3/part1/splitter/src/main.rs
+++ lab3/part1/splitter/src/main.rs
@@ -2,7 +2,7 @@ extern crate flatbuffers;
  
 #[allow(dead_code, unused_imports)]
 mod schema_generated;
-//use schema_generated;
+use schema_generated::tflite::*;
 
 use std::fs::File;
 use std::io::Read;
@@ -15,8 +15,8 @@ fn main() {
     let mut input_f = File::open("input/lite-model_movenet_singlepose_lightning_tflite_int8_4.tflite").unwrap();
     let mut buf = Vec::new();
     input_f.read_to_end(&mut buf).unwrap();
-    let mut upper = flatbuffers::root::<schema_generated::tflite::Model>(&buf).expect("invalid model").unpack();  
-    let mut lower = flatbuffers::root::<schema_generated::tflite::Model>(&buf).expect("invalid model").unpack();  
+    let mut upper = flatbuffers::root::<Model>(&buf).expect("invalid model").unpack();  
+    let mut lower = flatbuffers::root::<Model>(&buf).expect("invalid model").unpack();  
 
     // split operators
     let mut split_index = 0;
@@ -26,18 +26,82 @@ fn main() {
             break;
         }
     }
-    
-    let upper_subgraphs = upper.subgraphs.as_mut().unwrap();
-    let upper_ops_p = upper_subgraphs[0].operators.as_mut().unwrap();
-    *upper_ops_p = (upper_ops_p[..split_index]).to_vec();
 
+    
+    // Print all operators, tensors, buffers in original network for debugging
+    /* 
+    let mut i = 0;
+    for operator in upper_subgraphs[0].operators.as_ref().unwrap() {
+        println!("Operator: {:?}, {:?}", i, operator);
+        i += 1;
+    }
+    i=0;
+    for tensor in upper_subgraphs[0].tensors.as_ref().unwrap() {
+        println!("Tensor: {:?}, {:?}", i, tensor);
+        i += 1;
+    }
+    i = 0;
+    for buffer in upper.buffers.as_ref().unwrap(){
+        println!("Buffer: {:?}, {:?}", i, buffer);
+        i += 1;
+    }
+    */
+
+    
+    // split the operators
+    let upper_subgraphs = upper.subgraphs.as_mut().unwrap();
     let lower_subgraphs = lower.subgraphs.as_mut().unwrap();
-    let lower_ops_p = lower_subgraphs[0].operators.as_mut().unwrap();
-    *lower_ops_p = (lower_ops_p[split_index..]).to_vec();
+    *lower_subgraphs[0].operators.as_mut().unwrap() = upper_subgraphs[0].operators.as_mut().unwrap().split_off(split_index);
+
+    // add the dequant/quant operators
+    let dequant_out_buffer = BufferT::default(); //{data: Some(vec![0; 48*48*24*4])};
+    upper.buffers.as_mut().unwrap().push(dequant_out_buffer);
+    let quant_in_buffer = BufferT::default(); //{data: Some(vec![0; 48*48*24*4])};
+    lower.buffers.as_mut().unwrap().push(quant_in_buffer);
+
+    let mut dequant_out_tensor = TensorT::default();
+    dequant_out_tensor = TensorT {
+        shape: Some(vec![1,48,48,24]),
+        buffer: 335,
+        name: Some("StatefulPartitionedCall:0".to_string()),
+        ..dequant_out_tensor
+    };
+
+    let mut quant_in_tensor = TensorT::default();
+    quant_in_tensor = TensorT {
+        shape: Some(vec![1,48,48,24]),
+        buffer: 335,
+        name: Some("Cast".to_string()),
+        ..quant_in_tensor
+    };
+
+    upper_subgraphs[0].tensors.as_mut().unwrap().push(dequant_out_tensor);
+    lower_subgraphs[0].tensors.as_mut().unwrap().push(quant_in_tensor);
+
+    let mut dequant_op = OperatorT::default();
+    dequant_op = OperatorT {
+        opcode_index: 13,
+        inputs: Some(vec![255]),
+        outputs: Some(vec![333]),
+        ..dequant_op
+    };
+
+    let mut quant_op = OperatorT::default();
+    quant_op = OperatorT {
+        opcode_index: 1,
+        inputs: Some(vec![333]),
+        outputs: Some(vec![255]),
+        ..quant_op
+    };
+
+    upper_subgraphs[0].operators.as_mut().unwrap().push(dequant_op);
+    lower_subgraphs[0].operators.as_mut().unwrap().push(quant_op);
+
 
     // change inputs/outputs
-    *upper_subgraphs[0].outputs.as_mut().unwrap() = vec![81; 1];
-    *lower_subgraphs[0].inputs.as_mut().unwrap() = vec![0; 1];
+    *upper_subgraphs[0].outputs.as_mut().unwrap() = vec![333];
+    *lower_subgraphs[0].inputs.as_mut().unwrap() = vec![333];
+
 
     // done editing the split network, save to disk
     let mut upper_builder = flatbuffers::FlatBufferBuilder::new();
